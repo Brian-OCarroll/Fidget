1b10e196548919663eb33d3edc2cfe1efa058f6f
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-23 16:01:52 -0700

    added light-switch jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './scss/App.scss'
+function LightSwitch() {
+  // const [light, setLight] = useState([
+  //   {
+  //     on: false,
 
+  //   }
+  // ]);
+  
+  return (
+    <div className="light-switch-container" >
+      <input type="checkbox" id="light-switch" />
+      <label for="light-switch" id="light-switch-label">
+        <div class="screw"></div>
+        <div class="switch"></div>
+        <div class="screw"></div>
+      </label>
+      <div id="background"></div>
+    </div>
+  )
+}
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LightSwitch />
       </div>
     );
   }
