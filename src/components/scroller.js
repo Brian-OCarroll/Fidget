2bc9d052dd9f75a8da7ed53a9634259b39325379
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-24 20:36:20 -0700

    added fidget scroller component

---
+++ src/components/scroller.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../scss/scroller.scss'
+export default function Scroller(props) {
+  //add on scroll property
+  //add velocity
+  return (
+    <div className="container container--scroller" >
+      <div className="cube-side">
+        <div className="wheel">
+          <div className="flex notches">
+            <div className="notch --notch1"></div>
+            <div className="notch --notch2"></div>
+            <div className="notch --notch3"></div>
+            <div className="notch --notch4"></div>
+            <div className="notch --notch5"></div>
+        
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
