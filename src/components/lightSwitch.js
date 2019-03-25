59c5dfd886c947ba4567a64dde810241618160de
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-24 20:38:14 -0700

    reformatted classNames for efficiency

--- src/components/lightSwitch.js
+++ src/components/lightSwitch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../scss/lightswitch.scss'
 export default function LightSwitch(props) {
     // const [light, setLight] = useState([
     //   {
@@ -8,14 +9,15 @@ export default function LightSwitch(props) {
     // ]);
     
     return (
-      <div onClick={props.click} className="light-switch-container" >
-        <input type="checkbox" id="light-switch" />
+        //need to put the onclick on the input as the label also fires off an onclick
+        //can change later when getting rid of label and input
+      <div  className="container --container-lightswitch" >
+        <input onClick={props.click} type="checkbox" id="light-switch" />
         <label htmlFor="light-switch" id="light-switch-label">
           <div className="screw"></div>
           <div className="switch"></div>
           <div className="screw"></div>
         </label>
-        <div id="background"></div>
       </div>
     )
   }
