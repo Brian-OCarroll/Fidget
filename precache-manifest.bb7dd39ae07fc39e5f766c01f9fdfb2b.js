3b12dfa0be5db8460b7be8f5bae6a7f3db4d2739
Brian O'Carroll <bocarroll36@gmail.com>
2019-03-26 20:26:08 -0700

    Updates

---
+++ precache-manifest.bb7dd39ae07fc39e5f766c01f9fdfb2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6280378dcf4e6313cb4772e91fac0337",
+    "url": "/Brian-OCarroll/Fidget/static/media/scroll.6280378d.ogg"
+  },
+  {
+    "revision": "eb7e43ae0dff24b74f1de35a6ab128d7",
+    "url": "/Brian-OCarroll/Fidget/static/media/best.eb7e43ae.mp3"
+  },
+  {
+    "revision": "b2a408061a713dcbff2a",
+    "url": "/Brian-OCarroll/Fidget/static/js/runtime~main.6e7e9140.js"
+  },
+  {
+    "revision": "a7046ec4c4bd911f402e",
+    "url": "/Brian-OCarroll/Fidget/static/js/main.97b5f3f5.chunk.js"
+  },
+  {
+    "revision": "685f9dd24654749c5bf9",
+    "url": "/Brian-OCarroll/Fidget/static/js/2.da0e3110.chunk.js"
+  },
+  {
+    "revision": "a7046ec4c4bd911f402e",
+    "url": "/Brian-OCarroll/Fidget/static/css/main.7547550a.chunk.css"
+  },
+  {
+    "revision": "b08e16bb4910d06d41b4872662d62b96",
+    "url": "/Brian-OCarroll/Fidget/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Brian-OCarroll/Fidget/precache-manifest.bb7dd39ae07fc39e5f766c01f9fdfb2b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Brian-OCarroll/Fidget/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
